33dd8554e15d436d67c2e2bda0241b3ee9bc898c
igor.zap <ihor.zap@alphawebgroup.com>
2025-01-20 15:57:56 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout.js'
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="cat on the hat"
+        src="../images/medium.webp"
+      />
     </Layout>
   )
 }
