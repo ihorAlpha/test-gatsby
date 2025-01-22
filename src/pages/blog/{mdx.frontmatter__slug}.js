1796630a46237d1cce155da0fe599780c79c5571
igor.zap <ihor.zap@alphawebgroup.com>
2025-01-23 00:43:04 +0200

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,14 +1,47 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-const BlogPost = () => {
+const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle="Super Cool Blog Posts">
       <p>My blog post contents will go here (eventually).</p>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
+      {children}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`
 
 export const Head = () => <Seo title="Super Cool Blog Posts" />
 
